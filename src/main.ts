bc6bd6367c48699eb98ada3c024291cb3af5dba0
xarantolus <xarantolus@protonmail.com>
2021-09-11 08:02:12 +0200

    Log errors correctly

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import { env } from 'process'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { env } from "process";
 
 async function run(): Promise<void> {
   try {
-    const client = github.getOctokit(env.GITHUB_TOKEN || '')
+    const client = github.getOctokit(env.GITHUB_TOKEN || "");
 
-    console.log(env.GITHUB_EVENT_NAME)
-
-
+    console.log(env.GITHUB_EVENT_NAME);
   } catch (error) {
-    core.setFailed((error || { message: "Unknown error" }).message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
