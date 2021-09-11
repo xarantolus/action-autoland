e4295fb21bf7bbc6a61b687424fe4d7a78ebf2ae
xarantolus <xarantolus@protonmail.com>
2021-09-11 21:07:24 +0200

    Group output more nicely

--- dist/index.js
+++ dist/index.js
@@ -347,106 +347,107 @@ function authorHasPermission(association) {
 // - check if the current pull request checks have all passed, it's not a draft
 function checkPullRequest(client, pr) {
     return __awaiter(this, void 0, void 0, function* () {
-        console.log(`Checking PR ${github.context.repo.owner}/${github.context.repo.repo}#${pr.number}`);
-        if (pr.state !== "open") {
-            console.log("PR is not open, cannot proceed");
-            return;
-        }
-        if (pr.draft) {
-            console.log("PR is a draft, cannot proceed");
-            return;
-        }
-        var prInfo = {
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            issue_number: pr.number,
-            per_page: 100,
-        };
-        var comments = yield client.rest.issues.listComments(prInfo).catch((e) => {
-            throw new Error("Listing comments: " + e);
-        });
-        // Now get the LAST comment on the PR (or PR body itself) that contains a command
-        var cmd;
-        var parseCommandText = function (text) {
-            try {
-                var _cmd = comment_1.LandAfterCommand.parse(text);
-                if (_cmd.dependencies.length === 0) {
+        yield core.group(`Checking PR ${github.context.repo.owner}/${github.context.repo.repo}#${pr.number}`, () => __awaiter(this, void 0, void 0, function* () {
+            if (pr.state !== "open") {
+                console.log("PR is not open, cannot proceed");
+                return;
+            }
+            if (pr.draft) {
+                console.log("PR is a draft, cannot proceed");
+                return;
+            }
+            var prInfo = {
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                issue_number: pr.number,
+                per_page: 100,
+            };
+            var comments = yield client.rest.issues.listComments(prInfo).catch((e) => {
+                throw new Error("Listing comments: " + e);
+            });
+            // Now get the LAST comment on the PR (or PR body itself) that contains a command
+            var cmd;
+            var parseCommandText = function (text) {
+                try {
+                    var _cmd = comment_1.LandAfterCommand.parse(text);
+                    if (_cmd.dependencies.length === 0) {
+                        return false;
+                    }
+                    cmd = _cmd;
+                    return true;
+                }
+                catch (e) {
+                    console.log("Error while parsing comment: " + e + "\nBody:\n" + text);
                     return false;
                 }
-                cmd = _cmd;
-                return true;
+            };
+            if (pr.body && authorHasPermission(pr.author_association)) {
+                parseCommandText(pr.body);
             }
-            catch (e) {
-                console.log("Error while parsing comment: " + e + "\nBody:\n" + text);
+            comments.data.reverse().find((comment) => {
+                if (authorHasPermission(comment.author_association)) {
+                    return parseCommandText(comment.body || comment.body_text || "");
+                }
                 return false;
+            });
+            if (!cmd) {
+                console.log("pull request doesn't have any commands associated with it");
+                return;
             }
-        };
-        if (pr.body && authorHasPermission(pr.author_association)) {
-            parseCommandText(pr.body);
-        }
-        comments.data.reverse().find((comment) => {
-            if (authorHasPermission(comment.author_association)) {
-                return parseCommandText(comment.body || comment.body_text || "");
+            console.log(`Found command:\n${JSON.stringify(cmd, null, 4)}`);
+            // Check if all runs/checks for this PR are passed/green
+            var checks = yield client.rest.checks.listForRef({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                ref: pr.head.sha,
+            });
+            var checksNotOk = checks.data.check_runs.find((run) => {
+                // if it isn't neutral, successful or skipped, then we need to wait a bit longer
+                return !["neutral", "success", "skipped"].includes(run.conclusion || "");
+            });
+            if (checksNotOk) {
+                console.log(`Check ${checksNotOk.name} is not OK, it's state is ${checksNotOk.conclusion || "null"}`);
+                return;
             }
-            return false;
-        });
-        if (!cmd) {
-            console.log("pull request doesn't have any commands associated with it");
-            return;
-        }
-        console.log(`Found command:\n${JSON.stringify(cmd, null, 4)}`);
-        // Check if all runs/checks for this PR are passed/green
-        var checks = yield client.rest.checks.listForRef({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            ref: pr.head.sha,
-        });
-        var checksNotOk = checks.data.check_runs.find((run) => {
-            // if it isn't neutral, successful or skipped, then we need to wait a bit longer
-            return !(["neutral", "success", "skipped"].includes(run.conclusion || ""));
-        });
-        if (checksNotOk) {
-            console.log(`Check ${checksNotOk.name} is not OK, it's state is ${checksNotOk.conclusion || "null"}`);
-            return;
-        }
-        // Now we can check if the PR command is satisfied
-        var satisfied = true;
-        for (const dependency of cmd.dependencies) {
-            try {
-                satisfied = yield dependency.isSatisfied(client, prInfo.owner, prInfo.repo);
-            }
-            catch (e) {
-                console.log("error while checking satisfaction: " + e);
-                satisfied = false;
-            }
-            finally {
-                if (!satisfied) {
-                    console.log(`Not satisfied with ${JSON.stringify(dependency)}`);
-                    break;
+            // Now we can check if the PR command is satisfied
+            var satisfied = true;
+            for (const dependency of cmd.dependencies) {
+                try {
+                    satisfied = yield dependency.isSatisfied(client, prInfo.owner, prInfo.repo);
+                }
+                catch (e) {
+                    console.log("error while checking satisfaction: " + e);
+                    satisfied = false;
+                }
+                finally {
+                    if (!satisfied) {
+                        console.log(`Not yet satisfied with (at least) ${JSON.stringify(dependency)}`);
+                        break;
+                    }
                 }
             }
-        }
-        if (!satisfied) {
-            console.log("pull request is not yet satisfied");
-            return;
-        }
-        console.log("Dependency conditions are satisfied. Continuing with merge.");
-        var mergeMethod = core.getInput("merge-method", {
-            required: true,
-        });
-        yield client.rest.pulls.merge({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            pull_number: pr.number,
-            // cast to any, then typescript doesn't complain about assignability
-            merge_method: mergeMethod,
-        });
-        yield client.rest.issues.createComment({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            issue_number: pr.number,
-            body: "This pull request was automatically merged because all conditions from the last `autoland after` command were met.",
-        });
+            if (!satisfied) {
+                console.log("pull request is not yet satisfied");
+                return;
+            }
+            console.log("Dependency conditions are satisfied. Continuing with merge.");
+            var mergeMethod = core.getInput("merge-method", {
+                required: true,
+            });
+            yield client.rest.pulls.merge({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                pull_number: pr.number,
+                // cast to any, then typescript doesn't complain about assignability
+                merge_method: mergeMethod,
+            });
+            yield client.rest.issues.createComment({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                issue_number: pr.number,
+                body: "This pull request was automatically merged because all conditions from the last `autoland after` command were met.",
+            });
+        }));
     });
 }
 exports.checkPullRequest = checkPullRequest;
--- src/pr.ts
+++ src/pr.ts
@@ -32,137 +32,148 @@ export async function checkPullRequest(
     head: { sha: string };
   }
 ) {
-  console.log(
-    `Checking PR ${github.context.repo.owner}/${github.context.repo.repo}#${pr.number}`
-  );
+  await core.group(
+    `Checking PR ${github.context.repo.owner}/${github.context.repo.repo}#${pr.number}`,
+    async () => {
+      if (pr.state !== "open") {
+        console.log("PR is not open, cannot proceed");
+        return;
+      }
+      if (pr.draft) {
+        console.log("PR is a draft, cannot proceed");
+        return;
+      }
 
-  if (pr.state !== "open") {
-    console.log("PR is not open, cannot proceed");
-    return;
-  }
-  if (pr.draft) {
-    console.log("PR is a draft, cannot proceed");
-    return;
-  }
+      var prInfo = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
 
-  var prInfo = {
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
+        issue_number: pr.number,
+        per_page: 100,
+      };
 
-    issue_number: pr.number,
-    per_page: 100,
-  };
+      var comments = await client.rest.issues
+        .listComments(prInfo)
+        .catch((e) => {
+          throw new Error("Listing comments: " + e);
+        });
 
-  var comments = await client.rest.issues.listComments(prInfo).catch((e) => {
-    throw new Error("Listing comments: " + e);
-  });
+      // Now get the LAST comment on the PR (or PR body itself) that contains a command
+      var cmd: LandAfterCommand | null | undefined;
 
-  // Now get the LAST comment on the PR (or PR body itself) that contains a command
-  var cmd: LandAfterCommand | null | undefined;
+      var parseCommandText = function (text: string) {
+        try {
+          var _cmd = LandAfterCommand.parse(text);
+          if (_cmd.dependencies.length === 0) {
+            return false;
+          }
 
-  var parseCommandText = function (text: string) {
-    try {
-      var _cmd = LandAfterCommand.parse(text);
-      if (_cmd.dependencies.length === 0) {
+          cmd = _cmd;
+          return true;
+        } catch (e) {
+          console.log("Error while parsing comment: " + e + "\nBody:\n" + text);
+          return false;
+        }
+      };
+
+      if (pr.body && authorHasPermission(pr.author_association)) {
+        parseCommandText(pr.body);
+      }
+      comments.data.reverse().find((comment) => {
+        if (authorHasPermission(comment.author_association)) {
+          return parseCommandText(comment.body || comment.body_text || "");
+        }
         return false;
+      });
+
+      if (!cmd) {
+        console.log(
+          "pull request doesn't have any commands associated with it"
+        );
+        return;
       }
 
-      cmd = _cmd;
-      return true;
-    } catch (e) {
-      console.log("Error while parsing comment: " + e + "\nBody:\n" + text);
-      return false;
-    }
-  };
+      console.log(`Found command:\n${JSON.stringify(cmd, null, 4)}`);
 
-  if (pr.body && authorHasPermission(pr.author_association)) {
-    parseCommandText(pr.body);
-  }
-  comments.data.reverse().find((comment) => {
-    if (authorHasPermission(comment.author_association)) {
-      return parseCommandText(comment.body || comment.body_text || "");
-    }
-    return false;
-  });
+      // Check if all runs/checks for this PR are passed/green
+      var checks = await client.rest.checks.listForRef({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
 
-  if (!cmd) {
-    console.log("pull request doesn't have any commands associated with it");
-    return;
-  }
+        ref: pr.head.sha,
+      });
 
-  console.log(`Found command:\n${JSON.stringify(cmd, null, 4)}`);
+      var checksNotOk = checks.data.check_runs.find((run) => {
+        // if it isn't neutral, successful or skipped, then we need to wait a bit longer
+        return !["neutral", "success", "skipped"].includes(
+          run.conclusion || ""
+        );
+      });
 
-  // Check if all runs/checks for this PR are passed/green
-  var checks = await client.rest.checks.listForRef({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
+      if (checksNotOk) {
+        console.log(
+          `Check ${checksNotOk.name} is not OK, it's state is ${
+            checksNotOk.conclusion || "null"
+          }`
+        );
 
-    ref: pr.head.sha,
-  });
+        return;
+      }
 
-  var checksNotOk = checks.data.check_runs.find((run) => {
-    // if it isn't neutral, successful or skipped, then we need to wait a bit longer
-    return !["neutral", "success", "skipped"].includes(run.conclusion || "");
-  });
+      // Now we can check if the PR command is satisfied
 
-  if (checksNotOk) {
-    console.log(
-      `Check ${checksNotOk.name} is not OK, it's state is ${
-        checksNotOk.conclusion || "null"
-      }`
-    );
+      var satisfied = true;
 
-    return;
-  }
+      for (const dependency of cmd.dependencies) {
+        try {
+          satisfied = await dependency.isSatisfied(
+            client,
+            prInfo.owner,
+            prInfo.repo
+          );
+        } catch (e) {
+          console.log("error while checking satisfaction: " + e);
+          satisfied = false;
+        } finally {
+          if (!satisfied) {
+            console.log(
+              `Not yet satisfied with (at least) ${JSON.stringify(dependency)}`
+            );
+            break;
+          }
+        }
+      }
 
-  // Now we can check if the PR command is satisfied
-
-  var satisfied = true;
-
-  for (const dependency of cmd.dependencies) {
-    try {
-      satisfied = await dependency.isSatisfied(
-        client,
-        prInfo.owner,
-        prInfo.repo
-      );
-    } catch (e) {
-      console.log("error while checking satisfaction: " + e);
-      satisfied = false;
-    } finally {
       if (!satisfied) {
-        console.log(`Not satisfied with ${JSON.stringify(dependency)}`);
-        break;
+        console.log("pull request is not yet satisfied");
+        return;
       }
+
+      console.log(
+        "Dependency conditions are satisfied. Continuing with merge."
+      );
+
+      var mergeMethod = core.getInput("merge-method", {
+        required: true,
+      });
+
+      await client.rest.pulls.merge({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+
+        pull_number: pr.number,
+
+        // cast to any, then typescript doesn't complain about assignability
+        merge_method: <any>mergeMethod,
+      });
+
+      await client.rest.issues.createComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: pr.number,
+        body:
+          "This pull request was automatically merged because all conditions from the last `autoland after` command were met.",
+      });
     }
-  }
-
-  if (!satisfied) {
-    console.log("pull request is not yet satisfied");
-    return;
-  }
-
-  console.log("Dependency conditions are satisfied. Continuing with merge.");
-
-  var mergeMethod = core.getInput("merge-method", {
-    required: true,
-  });
-
-  await client.rest.pulls.merge({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-
-    pull_number: pr.number,
-
-    // cast to any, then typescript doesn't complain about assignability
-    merge_method: <any>mergeMethod,
-  });
-
-  await client.rest.issues.createComment({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: pr.number,
-    body:
-      "This pull request was automatically merged because all conditions from the last `autoland after` command were met.",
-  });
+  );
 }
